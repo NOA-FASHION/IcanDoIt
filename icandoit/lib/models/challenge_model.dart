a5f5cd360812bc6ae7ca7842b9db5c8b93ddc6e8
malmont <michel.almont@gmail.com>
2021-02-26 18:53:18 -0400

    premiere modification

--- icandoit/lib/models/challenge_model.dart
+++ icandoit/lib/models/challenge_model.dart
@@ -2,13 +2,31 @@ import 'package:flutter/foundation.dart';
 
 enum unity_challenge { haute, normal }
 
+class Challengemodel2 {
+  final String name;
+  final String tache;
+  final String description;
+  Challengemodel2(
+      {@required this.name, @required this.tache, @required this.description});
+
+  Challengemodel2.fromJSON(Map<String, dynamic> json)
+      : name = json['name'],
+        tache = json['tache'],
+        description = json['description'];
+
+  Map<String, dynamic> toJson() {
+    return {"name": name, "tache": tache, "description": description};
+  }
+}
+
 class ChallengeModel {
   final String name;
   final String description;
   final unity_challenge unity;
+  List<Challengemodel2> listeDeTache;
 
-  ChallengeModel(
-      {@required this.name, @required this.description, @required this.unity});
+  ChallengeModel({this.name, this.description, this.unity, this.listeDeTache});
+
   ChallengeModel.fromJSON(Map<String, dynamic> json)
       : name = json['name'],
         description = json['description'],
