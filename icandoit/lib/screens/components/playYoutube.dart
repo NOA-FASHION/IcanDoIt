d540d967f34af37fad2ffa3173d03bd3b28ceb2a
malmont <michel.almont@gmail.com>
2021-06-03 13:55:54 -0400

    webview youtube ok

--- icandoit/lib/screens/components/playYoutube.dart
+++ icandoit/lib/screens/components/playYoutube.dart
@@ -1,6 +1,24 @@
+// import 'package:flutter/material.dart';
+// import 'package:webview_flutter/webview_flutter.dart';
+// import 'dart:async';
+
+// class PlayYoutubeScreen extends StatefulWidget {
+//   final String nameChallenge;
+//   const PlayYoutubeScreen({Key key, this.nameChallenge}) : super(key: key);
+
+//   @override
+//   _PlayYoutubeScreenState createState() => _PlayYoutubeScreenState();
+// }
+
+// class _PlayYoutubeScreenState extends State<PlayYoutubeScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     final Completer<WebViewController> _controller =
+//         Completer<WebViewController>();
+
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'dart:async';
+import 'package:flutter_youtube_view/flutter_youtube_view.dart';
+import 'package:youtube_parser/youtube_parser.dart';
 
 class PlayYoutubeScreen extends StatefulWidget {
   final String nameChallenge;
@@ -10,52 +28,82 @@ class PlayYoutubeScreen extends StatefulWidget {
   _PlayYoutubeScreenState createState() => _PlayYoutubeScreenState();
 }
 
-class _PlayYoutubeScreenState extends State<PlayYoutubeScreen> {
+class _PlayYoutubeScreenState extends State<PlayYoutubeScreen>
+    implements YouTubePlayerListener {
+  double _currentVideoSecond = 0.0;
+  String _playerState = "";
+  FlutterYoutubeViewController _controller;
+
+  @override
+  void onCurrentSecond(double second) {
+    print("onCurrentSecond second = $second");
+    _currentVideoSecond = second;
+  }
+
+  @override
+  void onError(String error) {
+    print("onError error = $error");
+  }
+
+  @override
+  void onReady() {
+    print("onReady");
+  }
+
+  @override
+  void onStateChange(String state) {
+    print("onStateChange state = $state");
+    setState(() {
+      _playerState = state;
+    });
+  }
+
+  @override
+  void onVideoDuration(double duration) {
+    print("onVideoDuration duration = $duration");
+  }
+
+  void _onYoutubeCreated(FlutterYoutubeViewController controller) {
+    this._controller = controller;
+  }
+
+  void _loadOrCueVideo() {
+    _controller.loadOrCueVideo(
+        getIdFromUrl(widget.nameChallenge), _currentVideoSecond);
+  }
+
   @override
   Widget build(BuildContext context) {
-    final Completer<WebViewController> _controller =
-        Completer<WebViewController>();
-
-    return Container(
-      child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(100.0),
-          child: SafeArea(
-            child: AppBar(
-              centerTitle: true,
-              flexibleSpace: Container(
-                padding: EdgeInsets.only(right: 30.0),
-                alignment: Alignment.centerRight,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Image.asset(
-                      'assets/logo.png',
-                      width: 60,
-                    ),
-                  ],
-                ),
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.centerLeft,
-                        end: Alignment.centerRight,
-                        colors: <Color>[Colors.purple, Colors.blue])),
+    return Scaffold(
+        appBar: AppBar(title: const Text('Default UI')),
+        body: Stack(
+          children: <Widget>[
+            Container(
+                child: FlutterYoutubeView(
+              onViewCreated: _onYoutubeCreated,
+              listener: this,
+              params: YoutubeParam(
+                videoId: getIdFromUrl(widget.nameChallenge),
+                showUI: true,
+                startSeconds: 5 * 60.0,
+                showYoutube: false,
+                showFullScreen: false,
               ),
-            ),
-          ),
-        ),
-        body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: WebView(
-            initialUrl: widget.nameChallenge,
-            onWebViewCreated: (WebViewController webViewController) {
-              _controller.complete(webViewController);
-            },
-            javascriptMode: JavascriptMode.unrestricted,
-          ),
-        ),
-      ),
-    );
+            )),
+            Center(
+                child: Column(
+              children: <Widget>[
+                Text(
+                  'Current state: $_playerState',
+                  style: TextStyle(color: Colors.blue),
+                ),
+                RaisedButton(
+                  onPressed: _loadOrCueVideo,
+                  child: Text('Click reload video'),
+                ),
+              ],
+            ))
+          ],
+        ));
   }
 }
