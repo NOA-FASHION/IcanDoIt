7a9964e46631ce9c4f203f911df7dde44ebce6d0
malmont <michel.almont@gmail.com>
2021-10-05 21:32:40 -0400

    mise a jour pod update

---
+++ icandoit/ios/build/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/GeneratedPluginRegistrant.d
@@ -0,0 +1,22 @@
+dependencies: \
+  /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/Runner/GeneratedPluginRegistrant.m \
+  /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/Runner/GeneratedPluginRegistrant.h \
+  /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Debug-iphoneos/Flutter.framework/Modules/module.modulemap \
+  /Volumes/USB2/dossier\ sans\ titre\ 2/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.4.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
+  /Volumes/USB2/dossier\ sans\ titre\ 2/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.4.sdk/usr/include/mach-o/dyld.modulemap \
+  /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterViewController.h \
+  /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterPluginAppLifeCycleDelegate.h \
+  /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterHeadlessDartRunner.h \
+  /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterEngineGroup.h \
+  /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterEngine.h \
+  /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterDartProject.h \
+  /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterCallbackCache.h \
+  /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterTexture.h \
+  /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterPlatformViews.h \
+  /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterCodecs.h \
+  /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterChannels.h \
+  /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterBinaryMessenger.h \
+  /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterPlugin.h \
+  /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterMacros.h \
+  /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterAppDelegate.h \
+  /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Debug-iphoneos/Flutter.framework/Headers/Flutter.h
