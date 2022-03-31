21b454ee53ea3806306127b011af3056eba78f97
malmont <michel.almont@gmail.com>
2022-03-30 20:15:17 -0400

    maj ios

--- icandoit/ios/build/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/GeneratedPluginRegistrant.d
+++ icandoit/ios/build/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/GeneratedPluginRegistrant.d
@@ -2,8 +2,22 @@ dependencies: \
   /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/Runner/GeneratedPluginRegistrant.m \
   /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/Runner/GeneratedPluginRegistrant.h \
   /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Debug-iphoneos/Flutter.framework/Modules/module.modulemap \
-  /Volumes/USB2/dossier\ sans\ titre\ 2/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.4.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
-  /Volumes/USB2/dossier\ sans\ titre\ 2/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.4.sdk/usr/include/mach-o/dyld.modulemap \
+  /Volumes/DISK2/xcode\ 13/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.0.sdk/usr/include/kcdata.modulemap \
+  /Volumes/DISK2/xcode\ 13/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.0.sdk/usr/include/uuid.modulemap \
+  /Volumes/DISK2/xcode\ 13/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.0.sdk/usr/include/netinet6.modulemap \
+  /Volumes/DISK2/xcode\ 13/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.0.sdk/usr/include/machine.modulemap \
+  /Volumes/DISK2/xcode\ 13/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.0.sdk/usr/include/device.modulemap \
+  /Volumes/DISK2/xcode\ 13/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.0.sdk/usr/include/mach_debug.modulemap \
+  /Volumes/DISK2/xcode\ 13/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.0.sdk/usr/include/mach_machine.modulemap \
+  /Volumes/DISK2/xcode\ 13/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.0.sdk/usr/include/mach.modulemap \
+  /Volumes/DISK2/xcode\ 13/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.0.sdk/usr/include/bank.modulemap \
+  /Volumes/DISK2/xcode\ 13/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.0.sdk/usr/include/darwin_posix_sys_types.modulemap \
+  /Volumes/DISK2/xcode\ 13/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.0.sdk/usr/include/netinet.modulemap \
+  /Volumes/DISK2/xcode\ 13/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.0.sdk/usr/include/net.modulemap \
+  /Volumes/DISK2/xcode\ 13/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.0.sdk/usr/include/darwin_cdefs.modulemap \
+  /Volumes/DISK2/xcode\ 13/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.0.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
+  /Volumes/DISK2/xcode\ 13/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS15.0.sdk/usr/include/mach-o/dyld.modulemap \
+  /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/Pods/Firebase/CoreOnly/Sources/module.modulemap \
   /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterViewController.h \
   /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterPluginAppLifeCycleDelegate.h \
   /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Debug-iphoneos/Flutter.framework/Headers/FlutterHeadlessDartRunner.h \
---
+++ icandoit/ios/build/Runner.build/Debug-iphoneos/Runner.build/StaticAnalyzer/Runner/Runner/normal/arm64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Volumes/USB2/easy/Ican\ Do\ It/icandoit/ios/build/Runner.build/Debug-iphoneos/Runner.build/DerivedSources/Runner_vers.c
