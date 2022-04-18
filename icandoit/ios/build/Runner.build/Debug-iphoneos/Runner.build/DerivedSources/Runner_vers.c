c5dab8bdbb0360058c6427e5bee372a1c0726124
malmont <michel.almont@gmail.com>
2022-04-18 15:54:34 -0400

    ios maj

--- icandoit/ios/build/Runner.build/Debug-iphoneos/Runner.build/DerivedSources/Runner_vers.c
+++ icandoit/ios/build/Runner.build/Debug-iphoneos/Runner.build/DerivedSources/Runner_vers.c
@@ -1,5 +1,5 @@
  extern const unsigned char RunnerVersionString[];
  extern const double RunnerVersionNumber;
 
- const unsigned char RunnerVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Runner  PROJECT:Runner-1.4" "\n";
- const double RunnerVersionNumber __attribute__ ((used)) = (double)1.4;
+ const unsigned char RunnerVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Runner  PROJECT:Runner-1.7" "\n";
+ const double RunnerVersionNumber __attribute__ ((used)) = (double)1.7;
